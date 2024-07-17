09f246100e769f6ba886aed0979e74928a848660
awh <alanhaugen@gmail.com>
2024-07-17 18:20:12 +0000

    Can now add items to inbox

--- ajax.pl
+++ ajax.pl
@@ -5,12 +5,45 @@ use warnings;
 
 use JSON;
 use CGI;
+use DBI;
 
+# Setup variables to connect to DB
+my $data_source = q/DBI:mysql:organizer;127.0.0.1;3306/;
+my $user = q/root/;
+my $password = q//;
+
+# Connect to the data source and get a handle for that connection.
+my $dbh = DBI->connect($data_source, $user, $password)
+    or die "Can't connect to $data_source: $DBI::errstr";
+
+# Start ajax
 my $cgi = CGI->new;
 
 print $cgi->header('application/json;charset=UTF-8');
 
 my $id = $cgi->param('data_id');    
+
+my $sth = $dbh->prepare("INSERT INTO todos(message) VALUES('$id')")
+                   or die "prepare statement failed: $dbh->errstr()";
+
+$sth->execute() or die "execution failed: $dbh->errstr()"; 
+
+$sth = $dbh->prepare("SELECT todo_id FROM todos ORDER BY todo_id DESC limit 1")
+                   or die "prepare statement failed: $dbh->errstr()";
+
+$sth->execute() or die "execution failed: $dbh->errstr()"; 
+
+my $todo_id;
+
+$todo_id = $sth->fetchrow();
+
+$sth = $dbh->prepare("INSERT INTO inbox(todo_id) VALUES($todo_id)")
+                   or die "prepare statement failed: $dbh->errstr()";
+
+$sth->execute() or die "execution failed: $dbh->errstr()"; 
+
+$sth->finish();
+$dbh->disconnect();
 
 #convert  data to JSON
 my $op = JSON -> new -> utf8 -> pretty(1);
