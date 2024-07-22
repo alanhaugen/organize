485a577d790baa92ff94d4d2ccc8a9a04b4df64e
awh <alanhaugen@gmail.com>
2024-07-22 15:58:28 +0000

    Items can now be removed from all lists

--- delete.pl
+++ delete.pl
@@ -32,9 +32,17 @@ my $todo_id;
 
 $todo_id = $sth->fetchrow();
 
-$sth = $dbh->prepare("DELETE FROM inbox WHERE TODO_ID = $todo_id")
+$sth = $dbh->prepare("DELETE FROM inbox WHERE todo_id=$todo_id")
                    or die "prepare statement failed: $dbh->errstr()";
-
+$sth->execute() or die "execution failed: $dbh->errstr()"; 
+$sth = $dbh->prepare("DELETE FROM action WHERE todo_id=$todo_id")
+                   or die "prepare statement failed: $dbh->errstr()";
+$sth->execute() or die "execution failed: $dbh->errstr()"; 
+$sth = $dbh->prepare("DELETE FROM wait WHERE todo_id=$todo_id")
+                   or die "prepare statement failed: $dbh->errstr()";
+$sth->execute() or die "execution failed: $dbh->errstr()"; 
+$sth = $dbh->prepare("DELETE FROM long_term WHERE todo_id=$todo_id")
+                   or die "prepare statement failed: $dbh->errstr()";
 $sth->execute() or die "execution failed: $dbh->errstr()"; 
 
 $sth->finish();
